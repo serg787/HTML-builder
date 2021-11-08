309aad0a1bff8780d24ae92a4e9f51516ab3e953
serg787 <sipdom7878@gmail.com>
2021-11-08 22:15:03 +0300

    docs: update 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force:true}, (err) => {if (err) throw err;
 fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {if (err) throw err;});
 fs.readdir(path.join(__dirname, 'files'), (err, files) => {
   files.forEach(el => {
     fs.appendFile(path.join(__dirname, `files-copy/${el}`),'',(err) => {if (err) throw err;});
     fs.copyFile(path.join(__dirname, 'files', `${el}`), path.join(__dirname, 'files-copy', `${el}`), (err) => {if (err) throw err;});
   });
+});
 });
